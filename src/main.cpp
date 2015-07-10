2043bef1329edb0a32154447be084b6060a3b6f5
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2015-07-09 20:04:32 -0400

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,4 @@
-/*
-Questions/confusion:
-    - what's the difference between declaring these two ways:
-        QWidget window;
-        QVBoxLayout *layout = new QVBoxLayout();
-    - what's the difference between . and -> for addressing methods?
-*/
-
-
 #include <QApplication>
-
 #include "MainWindow.h"
 
 int main(int argc, char *argv[])
