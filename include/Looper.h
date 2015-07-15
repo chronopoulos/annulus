f32010adbf17061b10ec1b9d3604316bee698f83
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2015-07-15 14:16:15 -0400

    added getNextSample method, and setText(filename)

--- include/Looper.h
+++ include/Looper.h
@@ -8,13 +8,19 @@ class Looper : public QPushButton
     public:
         Looper();
         ~Looper();
+        bool loaded;
+        short getNextSample(void);
 
+    public slots:
+        void importFile(void);
+
+    private:
+        QString path;
         QString filename;
         int nframes;
         int nchannels;
         short* sampleBuffer;
-
-    public slots:
-        void importFile();
+        int nextIndex;
+        short tmpSample;
 
 };
--- src/Looper.cpp
+++ src/Looper.cpp
@@ -13,6 +13,8 @@ Looper::Looper() : QPushButton("(click to load loop)") {
 
     QObject::connect(this, SIGNAL(clicked(bool)), this, SLOT(importFile()));
 
+    loaded = false;
+
 }
 
 Looper::~Looper() {
@@ -21,13 +23,13 @@ Looper::~Looper() {
 
 void Looper::importFile(void) {
 
-    filename = QFileDialog::getOpenFileName(this, "Select Loop", "/home/chrono/music/samples");
+    path = QFileDialog::getOpenFileName(this, "Select Loop", "/home/chrono/music/samples");
 
-    if (!filename.isNull()) {
+    if (!path.isNull()) {
         SF_INFO info;
-        SNDFILE* sf = sf_open(filename.toStdString().c_str(), SFM_READ, &info);
+        SNDFILE* sf = sf_open(path.toStdString().c_str(), SFM_READ, &info);
         if (sf == NULL){
-            cout << "Could not open file: " << filename.toStdString() << endl;
+            cout << "Could not open file: " << path.toStdString() << endl;
             exit(-1);
         }
 
@@ -40,8 +42,23 @@ void Looper::importFile(void) {
             exit(-1);
         }
         sf_close(sf);
+        QStringList path_split = path.split("/");
+        filename = path_split.at(path_split.size()-1);
     }
 
-    this->setText("loaded");
+    nextIndex = 0;
+    loaded = true;
+    this->setText(filename);
+
+}
+
+short Looper::getNextSample(void) {
+
+    tmpSample = sampleBuffer[nextIndex];
+    if (++nextIndex == nframes*nchannels) {
+        nextIndex = 0;
+        cout << "Looper looped!" << endl;
+    }
+    return tmpSample;
 
 }
